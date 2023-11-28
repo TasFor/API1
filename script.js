8de59aa4a15056ad3414c09c180fe88ee5b1e7f5
TasFor <94999303+TasFor@users.noreply.github.com>
2023-11-28 22:41:12 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,199 +1,71 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function () {
-    const addButton = document.getElementById('add-task');
-    const newTaskInput = document.getElementById('new-task');
-    const searchBox = document.getElementById('search-box');
-    const todoList = document.getElementById('todo-list');
-    const taskDate = document.getElementById('task-date');
-    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-    
-
-    function addTask() {
-        const taskContent = newTaskInput.value;
-        const taskDeadline = taskDate.value;
-        
-        if (taskContent.length >= 3 && taskContent.length <= 255 && (!taskDeadline || new Date(taskDeadline) > new Date())) {
-            const newTask = {
-                id: Date.now(),  // Utwórz unikalne ID dla zadania
-                content: taskContent,
-                deadline: taskDeadline,
-                completed: false
-            };
-    
-            tasks.push(newTask);  // Dodaj nowe zadanie do tablicy zadań
-            saveTasks();
-            drawTasks();
-            newTaskInput.value = '';
-            taskDate.value = '';
-        } else {
-            alert('Zadanie musi mieć od 3 do 255 znaków i data musi być pusta lub w przyszłości.');
-        }
-    }
-    
-
-    function saveTasks() {
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-    }
-
-    function filterTasks() {
-        const searchTerm = searchBox.value.toLowerCase();
-        if (searchTerm.length >= 2) {
-            const filteredTasks = tasks.filter(task => task.content.toLowerCase().includes(searchTerm));
-            todoList.innerHTML = '';
-            filteredTasks.forEach((task, index) => {
-                const taskElement = document.createElement('li');
-                let taskContent = task.content;
-
-                const contentSpan = document.createElement('span');
-                contentSpan.textContent = task.content;
-                contentSpan.id = `content-${task.id}`;
-                contentSpan.addEventListener('click', () => editTask(task.id));
-        
-                // Span dla daty zadania z unikalnym ID.
-                const deadlineSpan = document.createElement('span');
-                deadlineSpan.textContent = formatDate(task.deadline);
-                deadlineSpan.id = `deadline-${task.id}`;
-                deadlineSpan.addEventListener('click', () => editTask(task.id));
-        
-                taskElement.appendChild(contentSpan);
-                taskElement.appendChild(deadlineSpan);
-    
-                // Wyróżnienie frazy
-                const regex = new RegExp(searchTerm, 'gi');
-                taskContent = taskContent.replace(regex, match => `<span class="highlight">${match}</span>`);
-                taskElement.innerHTML = `<span>${taskContent} <span class="task-date">(Termin: ${formatDate(task.deadline)})</span></span>`;
-
-                // // Przycisk edycji
-                // const editButton = document.createElement('button');
-                // editButton.textContent = 'Edytuj';
-                // editButton.className = 'edit';
-                // editButton.onclick = () => editTask(taskId); // Pamiętaj o zmianie tej linii, jeśli twoja logika wymaga innej implementacji
-                // taskElement.appendChild(editButton);
-    
-                // Przycisk usuwania
-                const deleteButton = document.createElement('button');
-                deleteButton.textContent = 'Usuń';
-                deleteButton.className = 'delete';
-                deleteButton.onclick = () => deleteTask(index); // Pamiętaj o zmianie tej linii, jeśli twoja logika wymaga innej implementacji
-                taskElement.appendChild(deleteButton);
-    
-                todoList.appendChild(taskElement);
-            });
-        } else {
-            drawTasks();
-        }
-    }
-
-    function drawTasks() {
-            todoList.innerHTML = ' ';
-            tasks.forEach((task, index) => {
-                const taskElement = document.createElement('li');
-                taskElement.innerHTML = `<span>${task.content} <span class="task-date">(Termin: ${formatDate(task.deadline)})</span></span>`;
+document.getElementById('weather-btn').addEventListener('click', function() {
+    const address = document.getElementById('address-input').value;
+    getCurrentWeather(address);
+    getForecast(address);
+  });
+  
+  function getCurrentWeather(address) {
+    const apiKey = '7ded80d91f2b280ec979100cc8bbba94';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${address}&appid=${apiKey}&units=metric`;
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.open('GET', url, true);
+    // xhr.addEventListener("load",()=>{
+    //     console.log(JSON.parse(xhr.responseText))
+    // })
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            const weatherData = xhr.response;
+            const currentDate = new Date().toLocaleString();
+            const temp = weatherData.main.temp.toFixed(2);
+            const feelsLike = weatherData.main.feels_like.toFixed(2);
+            const weatherDescription = weatherData.weather[0].description;
+            const iconUrl = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`;
             
-                const contentSpan = document.createElement('span');
-                contentSpan.textContent = task.content;
-                contentSpan.id = `content-${task.id}`;
-                contentSpan.addEventListener('click', () => editTask(task.id));
+            document.getElementById('current-weather').innerHTML = `
+              <div class="weather-entry">
+                <div class="weather-date">${currentDate}</div>
+                <div class="weather-icon"><img src="${iconUrl}" alt="${weatherDescription}"></div>
+                <div class="weather-temperature">Aktualna Temperatura: ${temp}°C (Odczuwalna: ${feelsLike}°C)</div>
+                <div class="weather-description">${weatherDescription}</div>
+              </div>
+            `;
+      } else {
+        document.getElementById('current-weather').textContent = 'Error fetching current weather.';
+      }
+    };
+    xhr.send();
+  }
+  
+function getForecast(address) {
+    const apiKey = '7ded80d91f2b280ec979100cc8bbba94';
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${address}&appid=${apiKey}&units=metric`;
+    
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        const forecastElement = document.getElementById('forecast');
+        forecastElement.innerHTML = ''; 
+        data.list.forEach(entry => {
+        const dateTime = new Date(entry.dt_txt).toLocaleString();
+        const temp = entry.main.temp.toFixed(2);
+        const feelsLike = entry.main.feels_like.toFixed(2);
+        const weatherDescription = entry.weather[0].description;
+        const iconUrl = `https://openweathermap.org/img/wn/${entry.weather[0].icon}.png`;
         
-                // Span dla daty zadania z unikalnym ID.
-                const deadlineSpan = document.createElement('span');
-                deadlineSpan.textContent = formatDate(task.deadline);
-                deadlineSpan.id = `deadline-${task.id}`;
-                deadlineSpan.addEventListener('click', () => editTask(task.id));
-        
-                taskElement.appendChild(contentSpan);
-                taskElement.appendChild(deadlineSpan);
-            // Przycisk edycji
-            // const editButton = document.createElement('button');
-            // editButton.textContent = 'Edytuj';
-            // editButton.className = 'edit';
-            // editButton.onclick = () => editTask(index);
-            // taskElement.appendChild(editButton);
-    
-            // Przycisk usuwania
-            const deleteButton = document.createElement('button');
-            deleteButton.textContent = 'Usuń';
-            deleteButton.className = 'delete';
-            deleteButton.onclick = () => deleteTask(index);
-            taskElement.appendChild(deleteButton);
-    
-            todoList.appendChild(taskElement);
-        });
-    }
-    
-    function deleteTask(index) {
-        tasks.splice(index, 1);
-        saveTasks();
-        drawTasks();
-    }
-
-    function formatDate(dateString) {
-        const options = { year: 'numeric', month: 'long', day: 'numeric' };
-        return new Date(dateString).toLocaleDateString( options);
-    }
-
-
-    function editTask(taskId) {
-        const taskIndex = tasks.findIndex(task => task.id === taskId);
-        if (taskIndex !== -1) {
-            const task = tasks[taskIndex];
-    
-            // Utwórz element input dla treści zadania.
-            const contentInput = document.createElement('input');
-            contentInput.type = 'text';
-            contentInput.value = task.content;
-            contentInput.className = 'edit-input'; // Dodaj klasę CSS dla stylizacji
-    
-            // Utwórz element input dla daty zadania.
-            const dateInput = document.createElement('input');
-            dateInput.type = 'date';
-            dateInput.value = task.deadline;
-            dateInput.className = 'edit-input'; // Dodaj klasę CSS dla stylizacji
-    
-            // Znajdź elementy DOM dla treści i daty zadania.
-            const contentSpan = document.querySelector(`#content-${task.id}`);
-            const deadlineSpan = document.querySelector(`#deadline-${task.id}`);
-    
-            // Zastąp elementy DOM polami input.
-            contentSpan.replaceWith(contentInput);
-            deadlineSpan.replaceWith(dateInput);
-    
-            // Skupienie na polu treści.
-            contentInput.focus();
-    
-            // Funkcja do zapisania zmian i przywrócenia widoku.
-            const saveChanges = () => {
-                const newContent = contentInput.value.trim();
-                const newDeadline = dateInput.value;
-                if (newContent && newDeadline) {
-                    task.content = newContent;
-                    task.deadline = newDeadline;
-                    saveTasks();
-                    drawTasks();
-                }
-            };
-    
-            // Dodanie obsługi zdarzeń do zapisania zmian po opuszczeniu pól input.
-            contentInput.addEventListener('blur', saveChanges);
-            dateInput.addEventListener('blur', saveChanges);
-    
-            // Opcjonalnie: Możesz chcieć zapisać zmiany po naciśnięciu klawisza Enter.
-            contentInput.addEventListener('keyup', (event) => {
-                if (event.key === 'Enter') {
-                    saveChanges();
-                }
-            });
-            dateInput.addEventListener('keyup', (event) => {
-                if (event.key === 'Enter') {
-                    saveChanges();
-                }
-            });
-        }
-    }
-
-
-    addButton.addEventListener('click', addTask);
-    searchBox.addEventListener('keyup', filterTasks);
-
-    drawTasks();
-});
+        forecastElement.innerHTML += `
+          <div class="weather-entry">
+            <div class="weather-date">${dateTime}</div>
+            <div class="weather-icon"><img src="${iconUrl}" alt="${weatherDescription}"></div>
+            <div class="weather-temperature">Temperatura: ${temp}°C (Odczuwalna: ${feelsLike}°C)</div>
+            <div class="weather-description">${weatherDescription}</div>
+          </div>
+        `;
+      });
+    })
+      .catch(() => {
+        document.getElementById('forecast').textContent = 'Error fetching forecast.';
+      });
+  }
+  
